7df6e760fde80835b38db600cc5b420612c26f92
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-10-14 10:41:15 -0700

    hash map testing

--- map.h
+++ map.h
@@ -1,75 +1,20 @@
 /** 
- * Copy pasted from https://github.com/rxi/map
+ * Copied from "The C Programming Language" section 6.6 (slightly edited
+ * function names / struct names)
  */
-
 
 #ifndef MAP_H
 #define MAP_H
-
 #include <string.h>
+#define HASHSIZE 101
 
-#define MAP_VERSION "0.1.0"
-
-struct map_node_t;
-typedef struct map_node_t map_node_t;
-
-typedef struct {
-  map_node_t **buckets;
-  unsigned nbuckets, nnodes;
-} map_base_t;
-
-typedef struct {
-  unsigned bucketidx;
-  map_node_t *node;
-} map_iter_t;
+typedef struct MapNode {
+  struct MapNode *next;
+  char *key;
+  int value;
+} MapNode;
 
 
-#define map_t(T)\
-  struct { map_base_t base; T *ref; T tmp; }
-
-
-#define map_init(m)\
-  memset(m, 0, sizeof(*(m)))
-
-
-#define map_deinit(m)\
-  map_deinit_(&(m)->base)
-
-
-#define map_get(m, key)\
-  ( (m)->ref = map_get_(&(m)->base, key) )
-
-
-#define map_set(m, key, value)\
-  ( (m)->tmp = (value),\
-    map_set_(&(m)->base, key, &(m)->tmp, sizeof((m)->tmp)) )
-
-
-#define map_remove(m, key)\
-  map_remove_(&(m)->base, key)
-
-
-#define map_iter(m)\
-  map_iter_()
-
-
-#define map_next(m, iter)\
-  map_next_(&(m)->base, iter)
-
-
-void map_deinit_(map_base_t *m);
-void *map_get_(map_base_t *m, const char *key);
-int map_set_(map_base_t *m, const char *key, void *value, int vsize);
-void map_remove_(map_base_t *m, const char *key);
-map_iter_t map_iter_(void);
-const char *map_next_(map_base_t *m, map_iter_t *iter);
-
-
-typedef map_t(void*) map_void_t;
-typedef map_t(char*) map_str_t;
-typedef map_t(int) map_int_t;
-typedef map_t(char) map_char_t;
-typedef map_t(float) map_float_t;
-typedef map_t(double) map_double_t;
-
+extern MapNode* get_HashMap(MapNode* hashMap[HASHSIZE], char* s);
+extern MapNode* set_HashMap(MapNode* hashMap[HASHSIZE], char* key, int value);
 #endif

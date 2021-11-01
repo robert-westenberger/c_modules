05954ab260fcb77a800cff0f8d846e4c8a10fc8a
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-10-31 19:38:09 -0700

    WIP generic sll implementation

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -1,16 +1,10 @@
-/** 
- * Separate Chaining map using linked lists.
- * From https://www.andreinc.net/2021/10/02/implementing-hash-tables-in-c-part-1
- * This is a naive implementation, it is not cache friendly since it use a linked list.
- */
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include "singly_linked_list.h"
 
-SinglyLinkedList *create_SinglyLinkedList(NodeKeyOps key_ops, NodeValOps val_ops)
+SinglyLinkedList *create_SinglyLinkedList(NodeDataOps data_ops)
 {
     SinglyLinkedList *list;
     list = malloc(sizeof(*list));
@@ -19,249 +13,81 @@ SinglyLinkedList *create_SinglyLinkedList(NodeKeyOps key_ops, NodeValOps val_ops
         fprintf(stderr, "malloc() failed in file %s at line # %d", __FILE__, __LINE__);
         exit(EXIT_FAILURE);
     }
-    list->size = 0;
-    list->capacity = HASH_CAPACITY_INIT;
-    list->key_ops = key_ops;
-    list->val_ops = val_ops;
-    list->buckets = malloc(list->capacity * sizeof(*(list->buckets)));
-    if (NULL == list->buckets)
-    {
-        fprintf(stderr, "malloc() failed in file %s at line # %d", __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < list->capacity; i++)
-    {
-        // Initially all the buckets are NULL
-        // Memory will be allocated for them when needed
-        list->buckets[i] = NULL;
-    }
+    list->head = NULL;
+    list->data_ops = data_ops;
+    
     return list;
+}
+
+void insert_SinglyLinkedList(SinglyLinkedList *list, const void *data) {
+    Node* node;
+    node = malloc(sizeof(Node*));
+    node->data = list->data_ops.cp(data);
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+}
+
+int sizeof_SinglyLinkedList(SinglyLinkedList *list) {
+    return list->size;
+}
+
+void deleteValue_SinglyLinkedList(SinglyLinkedList *list, const void *data)
+{
+    Node *temp = list->head, *prev;
+
+    /* head contains item to be deleted */
+    if (temp != NULL && list->data_ops.eq(temp->data, data))
+    {
+        list->head = temp->next;
+        list->size--;
+        free(temp);
+        return;
+    }
+
+    /* Search the list for the value to be deleted, 
+    keeping track of the previous node as it needs to be 
+    updated. */
+    while (temp != NULL && !list->data_ops.eq(temp->data, data))
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    /* Value was not in linked list */
+    if (temp == NULL)
+    {
+        return;
+    }
+    list->size--;
+    prev->next = temp->next;
+    free(temp); // Free memory
+}
+bool contains_SinglyLinkedList(SinglyLinkedList *list, const void *data) {
+  Node *ptr = list->head;
+  while (ptr) {
+      if (list->data_ops.eq(ptr->data, data)) {
+          return true;
+      }
+      ptr = ptr->next;
+  }
+  return false;
 }
 void free_SinglyLinkedList(SinglyLinkedList *list)
 {
-    Node *crt;
-    Node *next;
-    for (int i = 0; i < list->capacity; ++i)
-    {
-        // Free memory for each bucket
-        crt = list->buckets[i];
-        while (NULL != crt)
-        {
-            next = crt->next;
-            // Free memory for key and value
-            list->key_ops.free(crt->key, list->key_ops.arg);
-            list->val_ops.free(crt->val, list->val_ops.arg);
-            // Free the node
-            free(crt);
-            crt = next;
-        }
-    }
-    // Free the buckets and the hash structure itself
-    free(list->buckets);
-    free(list);
-}
-static void *getNode(SinglyLinkedList *list, const void *key)
-{
-    Node *result = NULL;
-    Node *crt = NULL;
-    uint32_t h;
-    size_t bucket_idx;
-
-    // We compute the hash of the key to check for it's existence
-    h = list->key_ops.hash(key, list->key_ops.arg);
-    // We use simple division hashing for determining the bucket
-    bucket_idx = h % list->capacity;
-    crt = list->buckets[bucket_idx];
-
-    while (NULL != crt)
-    {
-        // Iterated through the linked list found at the bucket
-        // to determine if the element is present or not
-        if (crt->hash == h && list->key_ops.eq(crt->key, key, list->val_ops.arg))
-        {
-            result = crt;
-            break;
-        }
-        crt = crt->next;
-    }
-
-    // If the while search performed in the while loop was successful,
-    // `result` contains the node
-    // otherwise it's NULL
-    return result;
+    // WIP
 }
 
-static void hashGrow(SinglyLinkedList *list)
-{
-    Node **new_buckets;
-    Node *crt;
-    size_t new_capacity;
-    size_t new_idx;
-
-    new_capacity = list->capacity * HASH_CAPACITY_MULT;
-    new_buckets = malloc(sizeof(*new_buckets) * new_capacity);
-    if (NULL == new_buckets)
-    {
-        fprintf(stderr, "Cannot resize buckets array. Hash table won't be resized.\n");
-        return;
-    }
-    for (int i = 0; i < new_capacity; ++i)
-    {
-        new_buckets[i] = NULL;
-    }
-
-    // Rehash
-    // For each bucket
-    for (int i = 0; i < list->capacity; i++)
-    {
-        // For each linked list
-        crt = list->buckets[i];
-        while (NULL != crt)
-        {
-            // Finding the new bucket
-            new_idx = crt->hash % new_capacity;
-            Node *cur = crt;
-            crt = crt->next;
-            cur->next = new_buckets[new_idx];
-            new_buckets[new_idx] = cur;
-        }
-    }
-
-    list->capacity = new_capacity;
-
-    // Free the old buckets
-    free(list->buckets);
-
-    // Update with the new buckets
-    list->buckets = new_buckets;
-}
-
-static uint32_t nodeNumCols(Node *node)
-{
-    uint32_t result = 0;
-    if (node)
-    {
-        while (node->next != NULL)
-        {
-            result++;
-            node = node->next;
-        }
-    }
-    return result;
-}
-
-void *get_SinglyLinkedList(SinglyLinkedList *list, const void *key)
-{
-    Node *result = NULL;
-    if (NULL != (result = getNode(list, key)))
-    {
-        return result->val;
-    }
-    return NULL;
-}
-void put_SinglyLinkedList(SinglyLinkedList *list, const void *key, const void *val)
-{
-    Node *crt;
-    size_t bucket_idx;
-    crt = getNode(list, key);
-    if (crt)
-    {
-        // Key already exists
-        // We need to update the value
-        list->val_ops.free(crt->val, list->val_ops.arg);
-        crt->val = val ? list->val_ops.cp(val, list->val_ops.arg) : 0;
-    }
-    else
-    {
-        // Key doesn't exist
-        // - We create a node
-        // - We add a node to the corresponding bucket
-        crt = malloc(sizeof(*crt));
-        if (NULL == crt)
-        {
-            fprintf(stderr, "malloc() failed in file %s at line # %d", __FILE__, __LINE__);
-            exit(EXIT_FAILURE);
-        }
-        crt->hash = list->key_ops.hash(key, list->key_ops.arg);
-        crt->key = list->key_ops.cp(key, list->key_ops.arg);
-        crt->val = list->val_ops.cp(val, list->val_ops.arg);
-
-        // Simple division hashing to determine the bucket
-        bucket_idx = crt->hash % list->capacity;
-        crt->next = list->buckets[bucket_idx];
-        list->buckets[bucket_idx] = crt;
-
-        // Element has been added successfully
-        list->size++;
-
-        // Grow if needed
-        if (list->size > list->capacity * HASH_GROWTH)
-        {
-            hashGrow(list);
-        }
+void print_SinglyLinkedList(SinglyLinkedList *list, void (*print_data)(const void *data)) {
+    Node *ptr = list->head;
+    while (ptr) {
+        print_data(ptr->data);
+        ptr = ptr->next;
     }
 }
-void print_SinglyLinkedList(SinglyLinkedList *hash, void (*print_key)(const void *key), void (*print_val)(const void *val))
-{
-    Node *crt;
+    /* String operations*/
 
-    printf("Hash Capacity: %lu\n", hash->capacity);
-    printf("Hash Size: %lu\n", hash->size);
-
-    printf("Hash Buckets:\n");
-    for (int i = 0; i < hash->capacity; i++)
-    {
-        crt = hash->buckets[i];
-        printf("\tbucket[%d]:\n", i);
-        while (NULL != crt)
-        {
-            printf("\t\thash=%" PRIu32 ", key=", crt->hash);
-            print_key(crt->key);
-            printf(", value=");
-            print_val(crt->val);
-            printf("\n");
-            crt = crt->next;
-        }
-    }
-}
-bool contains_SinglyLinkedList(SinglyLinkedList *hash, const void *key)
-{
-    return getNode(hash, key) ? true : false;
-}
-
-/* Gets number of collisions from the hash table*/
-uint32_t numCol_SinglyLinkedList(SinglyLinkedList *list)
-{
-    uint32_t result = 0;
-    for (int i = 0; i < list->capacity; ++i)
-    {
-        result += nodeNumCols(list->buckets[i]);
-    }
-    return result;
-}
-
-/* String operations*/
-static uint32_t ch_hash_fmix32(uint32_t h)
-{
-    h ^= h >> 16;
-    h *= 0x3243f6a9U;
-    h ^= h >> 16;
-    return h;
-}
-uint32_t string_hash(const void *data, void *arg)
-{
-    //djb2
-    uint32_t hash = (const uint32_t)5381;
-    const char *str = (const char *)data;
-    char c;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
-
-    return ch_hash_fmix32(hash);
-}
-void *string_cp(const void *data, void *arg)
+    void *string_cp(const void *data)
 {
     const char *input = (const char *)data;
     size_t input_length = strlen(input) + 1;
@@ -275,20 +101,19 @@ void *string_cp(const void *data, void *arg)
     strcpy(result, input);
     return result;
 }
-bool string_eq(const void *data1, const void *data2, void *arg)
+bool string_eq(const void *data1, const void *data2)
 {
     const char *str1 = (const char *)data1;
     const char *str2 = (const char *)data2;
     return !(strcmp(str1, str2)) ? true : false;
 }
-void string_free(void *data, void *arg)
+void string_free(void *data)
 {
     free(data);
 }
 void string_print(const void *data)
 {
-    printf("%s", (const char *)data);
+    printf("%s\n", (const char *)data);
 }
 
-NodeKeyOps key_ops_string = {string_hash, string_cp, string_free, string_eq, NULL};
-NodeValOps val_ops_string = {string_cp, string_free, string_eq, NULL};
+NodeDataOps data_ops_string = {string_cp, string_free, string_eq};
--- singly_linked_list.h
+++ singly_linked_list.h
@@ -7,53 +7,39 @@
 
 typedef struct Node
 {
-    void *key;
-    void *val;
+    void *data;
     struct Node *next;
 } Node;
 
-/* NodeKeyOps and NodeValOps are traits that allow users to 
-specify how the hash is computed, how its copied and how memory is managed. */
-typedef struct NodeKeyOps
-{
-    void *(*cp)(const void *data, void *arg);
-    void (*free)(void *data, void *arg);
-    bool (*eq)(const void *data1, const void *data2, void *arg);
-    void *arg;
-} NodeKeyOps;
 
-typedef struct NodeValOps
+typedef struct NodeDataOps
 {
-    void *(*cp)(const void *data, void *arg);
-    void (*free)(void *data, void *arg);
-    bool (*eq)(const void *data1, const void *data2, void *arg);
-    void *arg;
-} NodeValOps;
+    void *(*cp)(const void *data);
+    void (*free)(void *data);
+    bool (*eq)(const void *data1, const void *data2);
+} NodeDataOps;
 
 typedef struct SinglyLinkedList
-{
-    size_t capacity; /* total number of available buckets */
-    size_t size;     /* The total number of elements */
-    Node **buckets;
-    NodeKeyOps key_ops;
-    NodeValOps val_ops;
+{   
+    int size;
+    struct Node *head;
+    NodeDataOps data_ops;
 } SinglyLinkedList;
 
-extern SinglyLinkedList *create_SinglyLinkedList(NodeKeyOps key_ops, NodeValOps val_ops);
+extern SinglyLinkedList *create_SinglyLinkedList(NodeDataOps data_ops);
 extern void free_SinglyLinkedList(SinglyLinkedList *list);
-extern void *get_SinglyLinkedList(SinglyLinkedList *list, const void *key);
-extern bool contains_SinglyLinkedList(SinglyLinkedList *list, const void *key);
-extern void put_SinglyLinkedList(SinglyLinkedList *list, const void *key, const void *val);
-extern void print_SinglyLinkedList(SinglyLinkedList *list, void (*print_key)(const void *key), void (*print_val)(const void *val));
-
+extern int sizeof_SinglyLinkedList(SinglyLinkedList *list);
+extern bool contains_SinglyLinkedList(SinglyLinkedList *list, const void *data);
+extern void insert_SinglyLinkedList(SinglyLinkedList *list, const void *data);
+extern void deleteValue_SinglyLinkedList(SinglyLinkedList *list, const void *data);
+extern void print_SinglyLinkedList(SinglyLinkedList *list, void (*print_data)(const void *data));
 
 /* String Operations */
 
-void *string_cp(const void *data, void *arg);
-bool string_eq(const void *data1, const void *data2, void *arg);
-void string_free(void *data, void *arg);
+void *string_cp(const void *data);
+bool string_eq(const void *data1, const void *data2);
+void string_free(void *data);
 void string_print(const void *data);
 
-extern NodeKeyOps key_ops_string;
-extern NodeValOps val_ops_string;
+extern NodeDataOps data_ops_string;
 #endif
